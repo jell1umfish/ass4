1204b76d3485e3f95d81369945a9e92754179668
Lenovo <jell1umfish@gmail.com>
2023-05-15 02:10:42 +0600

    test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.util.Random;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        MyHashTable<MyTestingClass, Student> table = new MyHashTable<>();
+        Random random = new Random();
+        for (int i = 0; i < 10000; i++) {
+            MyTestingClass key = new MyTestingClass(Integer.toString(random.nextInt(1000)));
+            Student value = new Student(5, "Abc Defg");
+            table.put(key, value);
+        }
+        table.printBuckets();
     }
 }
