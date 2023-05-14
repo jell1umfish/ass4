e74a21d0ae0c7df7d768e68ed9db063a46eed6a9
Lenovo <jell1umfish@gmail.com>
2023-05-15 02:09:22 +0600

    student

---
+++ src/Student.java
@@ -0,0 +1,38 @@
+import java.util.Objects;
+
+class Student {
+    private int id;
+    private String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Student student = (Student) obj;
+        return id == student.id && Objects.equals(name, student.name);
+    }
+}
