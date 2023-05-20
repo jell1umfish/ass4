ba76fac7e6ed1c33a8d3ab2b4536baf57a1e2543
jell1umfish <116599724+jell1umfish@users.noreply.github.com>
2023-05-20 10:48:58 +0600

    Add files via upload
    defence files

--- Main.java
+++ Main.java
@@ -1,7 +1,26 @@
+import java.util.Random;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        MyHashTable<Integer, Integer> table = new MyHashTable<>();
+        Integer value = 1;
+        Integer key = 0;
+        for (int i = 0; i < 5; i++) {
+            table.put(i, value+i);
+        }
+        Integer[] getvalue = new Integer[table.size()];
+        MyHashTable<Integer, Integer> defence_table = new MyHashTable<>();
+        for(int as = 0; as<table.size(); as++){
+            getvalue[as] = table.get(as);
+        }
+        for(int j = 0; j<5; j++){
+            defence_table.put(j, getvalue[j]);
+        }
+        for(int a = 0; a<defence_table.size(); a++){
+            System.out.println(defence_table.get(a));
+        }
 
     }
 }
--- MyHashTable.java
+++ MyHashTable.java
@@ -1,15 +1,65 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class MyHashTable<K,V> {
-    private class HashNode<K, V>{
-        private K key;
-        private V value;
-        private HashNode<K, V> next;
-        public HashNode(K key, V value){
+    private static class Node<K, V> {
+        K key;
+        V value;
+
+        public Node(K key, V value) {
             this.key = key;
             this.value = value;
         }
-        public String toString(){
-            return "{" + key + " " + value + "}";
+    }
+    private ArrayList<ArrayList<Node<K, V>>> buckets;
+    private int size;
+    public MyHashTable() {
+        buckets = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            buckets.add(new ArrayList<>());
+        }
+        size = 0;
+    }
+    public void put(K key, V value) {
+        int hash = key.hashCode() % buckets.size();
+        ArrayList<Node<K, V>> bucket = buckets.get(hash);
+        for (Node<K, V> node : bucket) {
+            if (node.key.equals(key)) {
+                node.value = value;
+                return;
+            }
+        }
+        bucket.add(new Node<>(key, value));
+        size++;
+    }
+    public V get(K key) {
+        int hash = key.hashCode() % buckets.size();
+        ArrayList<Node<K, V>> bucket = buckets.get(hash);
+        for (Node<K, V> node : bucket) {
+            if (node.key.equals(key)) {
+                return node.value;
+            }
+        }
+        return null;
+    }
+    public void remove(K key) {
+        int hash = key.hashCode() % buckets.size();
+        ArrayList<Node<K, V>> bucket = buckets.get(hash);
+        for (Node<K, V> node : bucket) {
+            if (node.key.equals(key)) {
+                bucket.remove(node);
+                size--;
+                return;
+            }
         }
     }
-
+    public int size() {
+        return size;
+    }
+    public void printBuckets() {
+        for (int i = 0; i < buckets.size(); i++) {
+            ArrayList<Node<K, V>> bucket = buckets.get(i);
+            System.out.println( i + ": " + bucket.size());
+        }
+    }
 }
